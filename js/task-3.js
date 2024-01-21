2891815327fa9c8f1af9fb93e59fcda592da73fb
Orest-Stetsyk <62755155+Orest-Stetsyk@users.noreply.github.com>
2024-01-21 20:51:28 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,11 @@
-function getElementWidth(content, padding, border) {
-    return Number.parseFloat(content) + 2*Number.parseFloat(padding) + 2*Number.parseFloat(border);
+function checkForSpam(message) {
+    return message.toLowerCase().includes("sale") || message.toLowerCase().includes("spam");
 }
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
